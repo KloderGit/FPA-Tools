4d5847418d4434960b4352220c7afd7ddd7b52e5
KloderGit <kaligula3@gmail.com>
2016-02-03 18:07:45 +0300

    Первая валидация

---
+++ QuestionsProject/ValidationRules.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace QuestionsProject
+{
+    public class CountRule : ValidationRule
+    {
+
+        String _text = String.Empty;
+
+        public string Count
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo ci)
+        {
+            _text = (string)value;
+
+            if (String.IsNullOrWhiteSpace(_text))
+            {
+                return new ValidationResult(false,
+                  "Строка не может быть пустой!");
+            }
+            else
+            {
+                return new ValidationResult(true, null);
+            }
+        }
+    }
+}
